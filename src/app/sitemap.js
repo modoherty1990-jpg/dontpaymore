76ed39f5187843a44594bb25ba4afce415323a1f
Martin O'Doherty <martinodoherty@Martins-MacBook-Air.local>
2026-03-14 09:52:09 +1100

    sitemap and robots.txt

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://dontpaymore.com.au',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ]
+}
